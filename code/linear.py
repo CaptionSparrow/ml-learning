97afe8ef2053e291a2d0134e1d12b4db62308292
captionsparrow <xiongzhq1997@gmail.com>
2018-08-25 18:38:44 +0800

    linear

--- code/linear.py
+++ code/linear.py
@@ -1,2 +1,63 @@
 import numpy as np
 import pandas as pd
+
+#あ
+DAT = 'data/carbon_nanotubes.csv'
+CVN = 10
+LAM = 0.1
+#あ
+
+def regression(x, y, λ):
+	# Regulation term
+	r = λ * np.eye(x.shape[1])
+
+	# Compute weights
+	w = np.linalg.inv(x.T.dot(x) + r).dot(x.T).dot(y.reshape(y.shape[0], 1))
+
+	return w
+
+def validation(flds, k, λ):
+	for f in range(k):
+		# Regression for folder f
+		e = np.concatenate([flds[0][i] for i in range(k) if i != f])
+		b = np.ones([e.shape[0], 1])
+		X = np.concatenate([e, b], axis = 1)
+		Y = np.concatenate([flds[1][i] for i in range(k) if i != f])
+		w = regression(X, Y, λ)
+
+		# Validation for folder f
+
+	return None
+
+def fold_data(egb, ltg, k):
+	# Shuffle the data
+	schlurfen = np.random.permutation(egb.shape[0])
+	egb = egb[schlurfen]
+	ltg = ltg[schlurfen]
+
+	# Fold k validations
+	Größe = egb.shape[0] // k
+	cuts = [Größe * i for i in range(1, k)]
+	egbs = np.split(egb, cuts)
+	ltgs = np.split(ltg, cuts)
+
+	return egbs, ltgs
+
+def main():
+	# Read data
+	dataset = pd.read_csv(DAT)
+
+	data_eingeben = dataset.iloc[:,:-1].values
+	data_leistung = dataset.iloc[:, -1].values
+
+	eingeben = np.array(data_eingeben)
+	leistung = np.array(data_leistung)
+
+	# Cross Validation
+	folders = fold_data(eingeben, leistung, CVN)
+	results = validation(folders, CVN, LAM)
+
+#あ
+if __name__ == '__main__':
+	main()
+#あ
